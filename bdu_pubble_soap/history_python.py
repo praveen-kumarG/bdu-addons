48603c241006c68a952c30de22ac89ab125e1662
Willem Hulshof <w.hulshof@magnus.nl>
2017-10-02 15:26:17 +0200

    first working call

--- bdu_pubble_soap/history_python.py
+++ bdu_pubble_soap/history_python.py
@@ -1,14 +1,79 @@
+import sys
+sys.path[0:0] = [
+  '/Users/WH/.voodoo/shared/eggs/nose-1.3.7-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/XlsxWriter-1.0.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/Unidecode-0.4.21-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/Pillow-4.2.1-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/phonenumbers-8.8.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/unicodecsv-0.14.1-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/watchdog-0.8.3-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/passlib-1.7.1-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pyotp-2.2.6-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/cachetools-2.0.1-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pyPostcode-0.3-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/xlrd-1.0.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/suds_jurko-0.6-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pyinotify-0.9.6-py2.7.egg',
+  '/workspace/a.r.odoo',
+  '/Users/WH/.voodoo/shared/eggs/Babel-2.5.1-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/decorator-4.1.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/docutils-0.14-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/feedparser-5.2.1-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/gevent-1.2.2-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/Jinja2-2.9.6-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/lxml-4.0.0-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/Mako-1.0.7-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/mock-2.0.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/ofxparse-0.16-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/psutil-5.3.1-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/psycogreen-1.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/psycopg2-2.7.3.1-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/Python_Chart-1.39-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pydot-1.0.28-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pyparsing-2.2.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pyPdf-1.13-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pyserial-3.4-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/python_dateutil-2.4.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/python_ldap-2.4.44-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/python_openid-2.2.5-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pytz-2017.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/PyYAML-3.12-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/qrcode-5.3-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/reportlab-3.4.0-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/requests-2.18.4-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/vatnumber-1.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/vobject-0.9.5-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/Werkzeug-0.12.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/xlwt-1.3.0-py2.7.egg',
+  '/workspace/parts/odoo',
+  '/Users/WH/.voodoo/shared/eggs/python_stdnum-1.7-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/certifi-2017.7.27.1-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/urllib3-1.22-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/idna-2.6-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/chardet-3.0.4-py2.7.egg',
+  '/usr/lib/python2.7/dist-packages',
+  '/Users/WH/.voodoo/shared/eggs/olefile-0.44-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/beautifulsoup4-4.6.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pbr-2.0.0-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/funcsigs-1.0.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/MarkupSafe-1.0-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/greenlet-0.4.12-py2.7-linux-x86_64.egg',
+  '/Users/WH/.voodoo/shared/eggs/zc.buildout-2.9.5-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/zc.recipe.egg-2.0.4-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/pathtools-0.1.2-py2.7.egg',
+  '/Users/WH/.voodoo/shared/eggs/argh-0.26.2-py2.7.egg',
+  ]
 
 
 from suds.client import Client
-client = Client("file:///workspace/SalesWebService [13-9-2017].wsdl")
+client = Client("https://ws.pubble.nl/Sales.svc?singleWsdl")
 
 SalesOrder = client.factory.create('ns1:salesOrder')
-transmissionID = 257018651 # hoe komen we aan dit ID?
+transmissionID = 300000003 # hoe komen we aan dit ID?
 publisher = "testbdudata" #Is dit altijd dezelfde?
 apiKey = "9tituo3t2qo4zk7emvlb" # Endeze?
 
-SalesOrder.extOrderID = 5890148 #Kan dit ook alfanumeriek zijn?
+SalesOrder.extOrderID = 5890149 #Kan dit ook alfanumeriek zijn?
 SalesOrder.reference = "Advertentie Kwart Voorpagina Alkmaars en Schager Weekblad"
 SalesOrder.createdBy = "Barry"
 SalesOrder.debtor.extAccountingID = 3406
@@ -41,9 +106,6 @@ ad1.status = "active"
 SalesOrder.orderLine_Ads.adPlacement.append(ad1)
 
 response = client.service.processOrder(SalesOrder, transmissionID, publisher, apiKey)
+print response
 
-from suds.plugin import MessagePlugin
 
-client1 = Client("http://ws.pubble.nl/Sales.svc/?wsdl")
-client1 = Client("http://pubble.nl/Webservice/ISales/processOrder?wsdl")
-client1 = Client("http://pubble.nl/Webservice/ISales/processOrder/?wsdl")
--- bdu_pubble_soap/models/sale_advertising.py
+++ bdu_pubble_soap/models/sale_advertising.py
@@ -26,6 +26,7 @@ from odoo.exceptions import UserError
 from suds.client import Client
 from odoo.addons.queue_job.job import job, related_action
 from odoo.addons.queue_job.exception import FailedJobError
+import datetime
 
 
 class SaleOrder(models.Model):
@@ -46,25 +47,34 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_pubble(self):
+        # self.with_delay().transfer_order_to_pubble()
         self.transfer_order_to_pubble()
         return self.write({'state': 'pubble',
                            'date_sent_pubble': fields.Date.context_today(self)})
 
+#class SofromOdootoPubble(models.TransientModel):
+#    _name = 'sofrom.odooto.pubble'
+
+    @api.multi
+    @job
     def transfer_order_to_pubble(self):
-        client = Client("file:///workspace/SalesWebService [13-9-2017].wsdl")
+        #import pdb; pdb.set_trace()
+        ad = 0
+        client = Client("https://ws.pubble.nl/Sales.svc?singleWsdl")
         SalesOrder = client.factory.create('ns1:salesOrder')
-        transmissionID = 257018651
+        transmissionID = 300000009
         publisher = "testbdudata"
         apiKey = "9tituo3t2qo4zk7emvlb"
-        SalesOrder.extOrderID = self.name
+        SalesOrder.extOrderID = int(float(self.name))
         SalesOrder.reference = self.opportunity_subject
         SalesOrder.createdBy = self.user_id.name
-        SalesOrder.debtor.extAccountingID = self.published_customer.ref
-        SalesOrder.debtor.extDebtorID = self.published_customer.ref
-        SalesOrder.debtor.addedDate = self.published_customer.create_date
+        SalesOrder.debtor.extAccountingID = int(float(self.published_customer.ref))
+        SalesOrder.debtor.extDebtorID = int(float(self.published_customer.ref))
+        SalesOrder.debtor.addedBy = self.published_customer.create_uid.name
+        SalesOrder.debtor.addedDate = datetime.datetime.strptime(self.published_customer.create_date,'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
         SalesOrder.debtor.city = self.published_customer.city
         SalesOrder.debtor.emailadres = self.published_customer.email
-        SalesOrder.debtor.lastModified = self.published_customer.write_date
+        SalesOrder.debtor.lastModified = datetime.datetime.strptime(self.published_customer.write_date,'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
         SalesOrder.debtor.name = self.published_customer.name
         SalesOrder.debtor.postalcode = self.published_customer.zip
         if not self.advertising_agency:
@@ -72,23 +82,23 @@ class SaleOrder(models.Model):
         else:
             SalesOrder.agency.extAccountingID = self.advertising_agency.ref
             SalesOrder.agency.extDebtorID = self.advertising_agency.ref
-            SalesOrder.agency.addedDate = self.advertising_agency.create_date
+            SalesOrder.agency.addedDate = datetime.datetime.strptime(self.advertising_agency.create_date,'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
             SalesOrder.agency.city = self.advertising_agency.city
             SalesOrder.agency.emailadres = self.advertising_agency.email
-            SalesOrder.agency.lastModified = self.advertising_agency.write_date
+            SalesOrder.agency.lastModified = datetime.datetime.strptime(self.advertising_agency.write_date,'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
             SalesOrder.agency.name = self.advertising_agency.name
             SalesOrder.agency.postalcode = self.advertising_agency.zip
         for line in self.order_line:
             ad += 1
             ad = client.factory.create('ns1:adPlacement')
-            ad.adSize.adTypeName = "Advertentiepagina"
-            ad.adSize.extAdSizeID = 4005
+            ad.adSize.adTypeName = line.ad_class.name
+            ad.adSize.extAdSizeID = int(float(line.product_id.default_code))
             ad.adSize.height = 200
-            ad.adSize.name = "KWARTADVVP"
+            ad.adSize.name = line.product_id.name
             ad.adSize.width = 522
-            ad.edition.editionDate = "2017-09-27T00:00:00"
-            ad.edition.extPublicationID = line.title
-            ad.extPlacementID = 14890147
+            ad.edition.editionDate = datetime.datetime.strptime(line.adv_issue.issue_date,'%Y-%m-%d').strftime('%Y-%m-%dT%H:%M:%S.%f')
+            ad.edition.extPublicationID = line.title.name
+            ad.extPlacementID = line.id
             ad.price = 0
             ad.productionDetail.color = "true"
             ad.productionDetail.isClassified = "false"
@@ -96,6 +106,9 @@ class SaleOrder(models.Model):
             ad.productionDetail.placementComments = line.name
             ad.status = "active"
             SalesOrder.orderLine_Ads.adPlacement.append(ad)
-
+        print transmissionID
+        print SalesOrder
         response = client.service.processOrder(SalesOrder, transmissionID, publisher, apiKey)
+        print response
         return True
+
