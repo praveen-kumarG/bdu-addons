6ad1003c278cea311c58aab5e958bc1d6186eb53
Willem Hulshof <w.hulshof@magnus.nl>
2017-12-19 20:32:47 +0100

    Adaptations of bdu-account: report_invoice.xml and

--- bdu_account/models/account_invoice.py
+++ bdu_account/models/account_invoice.py
@@ -7,7 +7,7 @@ import odoo.addons.decimal_precision as dp
 class AccountInvoice(models.Model):
     _inherit = ["account.invoice"]
 
-    published_customer = fields.Many2one('res.partner', 'Advertiser', domain=[('customer','=',True)])
+#    published_customer = fields.Many2one('res.partner', 'Advertiser', domain=[('customer','=',True)])
 
 
     # TODO: check if this is needed?
@@ -20,11 +20,11 @@ class AccountInvoice(models.Model):
         return self.env['report'].get_action(self, 'account.report_invoice')
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = ["account.invoice.line"]
+#class AccountInvoiceLine(models.Model):
+#    _inherit = ["account.invoice.line"]
 
-    so_number = fields.Char(string='Order Number')
-    sol_issuedt = fields.Date(string='Issue Date')
-    sol_title = fields.Char(string='Title')
-    sol_discount = fields.Float(string='Discount', digits=dp.get_precision('Account'))
-    sol_computed_discount = fields.Float(string='Computed Discount', digits=dp.get_precision('Account'))
+#    so_number = fields.Char(string='Order Number')
+#    sol_issuedt = fields.Date(string='Issue Date')
+#    sol_title = fields.Char(string='Title')
+#    sol_discount = fields.Float(string='Discount', digits=dp.get_precision('Account'))
+#    sol_computed_discount = fields.Float(string='Computed Discount', digits=dp.get_precision('Account'))
