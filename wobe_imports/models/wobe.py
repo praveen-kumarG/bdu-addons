67236673795ade0ad578f7d69f9ab750b70723d8
kumarinie <kumarisushmanie@gmail.com>
2017-12-21 17:09:24 +0530

    Picking create partial Commit, Stock move under progress

--- wobe_imports/models/wobe.py
+++ wobe_imports/models/wobe.py
@@ -649,23 +649,6 @@ class Job(models.Model):
                     'partner_id': picking.partner_id.id
                 })
             case.write({'picking_id':picking.id,'state':'picking_created'})
-            # if any([ptype in ['product', 'consu'] for ptype in order.order_line.mapped('product_id.type')]):
-            #     pickings = order.picking_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
-            #     if not pickings:
-            #         res = order._prepare_picking()
-            #         picking = StockPicking.create(res)
-            #     else:
-            #         picking = pickings[0]
-            #     moves = order.order_line._create_stock_moves(picking)
-            #     moves = moves.filtered(lambda x: x.state not in ('done', 'cancel')).action_confirm()
-            #     seq = 0
-            #     for move in sorted(moves, key=lambda move: move.date_expected):
-            #         seq += 5
-            #         move.sequence = seq
-            #     moves.force_assign()
-            #     picking.message_post_with_view('mail.message_origin_link',
-            #                                    values={'self': picking, 'origin': order},
-            #                                    subtype_id=self.env.ref('mail.mt_note').id)
         return True
 
     @api.model
