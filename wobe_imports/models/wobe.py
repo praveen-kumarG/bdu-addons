baf2c05c04f50fa97f44e966638798785fedebdd
Willem Hulshof <w.hulshof@magnus.nl>
2018-02-07 16:02:41 +0100

    mass calculation per roll: width/total_width iso number/total_number

--- wobe_imports/models/wobe.py
+++ wobe_imports/models/wobe.py
@@ -800,14 +800,15 @@ class Job(models.Model):
         ratioSum = {}
         for roll in job.paper_product_ids:
             mass, width = _get_MassWidth(roll.product_id)
-            number = int(roll.number_rolls)
+#            number = int(roll.number_rolls)
 
             if mass not in ratioSum:
-                ratioSum[mass] = {'number_mass': number}
+                ratioSum[mass] = {'width_mass_total': width}
             if width not in ratioSum:
-                ratioSum[mass][width] = {'number_width': number}
+                ratioSum[mass][width] = {'width': width}
             else:
-                ratioSum[mass]['number_mass'] += number
+                ratioSum[mass]['width_mass_total'] += width
+#                ratioSum[mass][width]['width_mass'] += width
 
 
         # Total Mass per PaperMass
@@ -823,8 +824,8 @@ class Job(models.Model):
         # Paper Rolls
         for roll in job.paper_product_ids:
             mass, width = _get_MassWidth(roll.product_id)
-            num_mass = ratioSum[mass]['number_mass']
-            num_width = ratioSum[mass][width]['number_width']
+            num_mass = ratioSum[mass]['width_mass_total']
+            num_width = ratioSum[mass][width]['width']
 
             # Net Production: (in Kg)
             NetMass = MassPerUnit.get(mass, 0) * job.net_quantity / 1000.0
