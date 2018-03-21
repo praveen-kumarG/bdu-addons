e344109cf12964e3d8b43eadd14143da7b5d8b03
Willem Hulshof <w.hulshof@magnus.nl>
2018-03-21 18:12:07 +0100

    sale_line_ids and origin visible in tree and form order line

--- bdu_account/__manifest__.py
+++ bdu_account/__manifest__.py
@@ -56,6 +56,7 @@ This module adds customization as per BDU Media
          "views/res_company_view.xml",
          "views/res_partner_view.xml",
          "views/product_view.xml",
+         "views/account_invoice_view.xml",
     ],
     'demo' : [],
     'installable': True
