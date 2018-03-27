bc7b6212e584bd470c209175cc7d523484a75419
Willem Hulshof <w.hulshof@magnus.nl>
2018-03-27 22:16:39 +0200

    improvements invoice confirmation in job queue

--- bdu_account/wizard/account_invoice_state.py
+++ bdu_account/wizard/account_invoice_state.py
@@ -13,12 +13,13 @@ class AccountInvoiceConfirm(models.TransientModel):
 
     chunk_size = fields.Integer('Chunk Size Job Queue', default=50)
     job_queue = fields.Boolean('Process via Job Queue', default=False)
+    execution_datetime = fields.Datetime('Job Execution not before', default=fields.Datetime.now())
 
     @api.multi
     def invoice_confirm(self):
         if not self.job_queue:
             return super(AccountInvoiceConfirm, self).invoice_confirm()
-
+#        eta = fields.Datetime.from_string(self.execution_datetime)
         context = dict(self._context or {})
         active_ids = context.get('active_ids', []) or []
         self.with_delay()._split_jobs(active_ids)
@@ -28,34 +29,19 @@ class AccountInvoiceConfirm(models.TransientModel):
     @job
     def _split_jobs(self, inv_ids):
         size = self.chunk_size
+        eta = fields.Datetime.from_string(self.execution_datetime)
         for x in xrange(0, len(inv_ids), size):
             chunk = inv_ids[x:x + size]
-            self.with_delay().invoice_confirm_jobqueue(chunk)
+            self.with_delay(eta=eta).invoice_confirm_jobqueue(chunk)
 
     @job
     @api.multi
     def invoice_confirm_jobqueue(self, chunk):
         for record in self.env['account.invoice'].browse(chunk):
             if record.state not in ('draft', 'proforma', 'proforma2'):
-                raise FailedJobError(_("Selected invoice(s) cannot be confirmed as they are not in 'Draft' or 'Pro-Forma' state."))
+                raise FailedJobError(_("Selected invoice(s) cannot be confirmed "
+                                       "as they are not in 'Draft' or 'Pro-Forma' state. "
+                                       "Reference/Description: '%s'  "
+                                       "Number: '%s'") % (record.name, record.number))
             record.action_invoice_open()
 
-'''class AccountInvoiceCancel(models.TransientModel):
-    """
-    This wizard will cancel the all the selected invoices.
-    If in the journal, the option allow cancelling entry is not selected then it will give warning message.
-    """
-
-    _name = "account.invoice.cancel"
-    _description = "Cancel the Selected Invoices"
-
-    @api.multi
-    def invoice_cancel(self):
-        context = dict(self._context or {})
-        active_ids = context.get('active_ids', []) or []
-
-        for record in self.env['account.invoice'].browse(active_ids):
-            if record.state in ('cancel', 'paid'):
-                raise UserError(_("Selected invoice(s) cannot be cancelled as they are already in 'Cancelled' or 'Done' state."))
-            record.action_invoice_cancel()
-        return {'type': 'ir.actions.act_window_close'}'''
