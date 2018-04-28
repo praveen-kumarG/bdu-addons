499ae11ece1de8c1fa5ebf7be5f73614036f8ba3
Willem Hulshof <w.hulshof@magnus.nl>
2018-04-28 17:09:10 +0200

    invoice/credit note layout and related link to sale order line

--- bdu_account/models/account_invoice.py
+++ bdu_account/models/account_invoice.py
@@ -8,7 +8,6 @@ import datetime
 class AccountInvoice(models.Model):
     _inherit = ["account.invoice"]
 
-#    published_customer = fields.Many2one('res.partner', 'Advertiser', domain=[('customer','=',True)])
 
 
     # TODO: check if this is needed?
@@ -17,12 +16,38 @@ class AccountInvoice(models.Model):
         """ Print the invoice and mark it as sent
         """
         self.ensure_one()
-        self.sent = True
-        return self.env['report'].get_action(self, 'account.report_invoice')
+        if self.type == 'out_refund':
+            for line in self.invoice_line_ids:
+                if not line.so_line_id:
+                    line.so_line_id = line.origin_line_ids.sale_line_ids.id
+        return super(AccountInvoice, self).invoice_print()
 
     def _get_refund_common_fields(self):
         res = super(AccountInvoice, self)._get_refund_common_fields()
         return res+['published_customer']
+
+    @api.model
+    def _refund_cleanup_lines(self, lines):
+        if self.env.context.get('mode') == 'modify':
+            result = super(AccountInvoice, self).with_context(mode=False)._refund_cleanup_lines(lines)
+            for i in xrange(0, len(lines)):
+                for name, field in lines[i]._fields.iteritems():
+                    if name == 'so_line_id' and not lines[i][name]:
+                        result[i][2][name] = lines[i]['sale_line_ids'].id
+                        lines[i][name] = lines[i]['sale_line_ids'].id
+                    if name == 'sale_line_ids':
+                        result[i][2][name] = [(6, 0, lines[i][name].ids)]
+                        lines[i][name] = False
+        else:
+            result = super(AccountInvoice, self)._refund_cleanup_lines(lines)
+            for i in xrange(0, len(lines)):
+                for name, field in lines[i]._fields.iteritems():
+                    if name == 'so_line_id' and not lines[i][name]:
+                        result[i][2][name] = lines[i]['sale_line_ids'].id
+                        lines[i][name] = lines[i]['sale_line_ids'].id
+        return result
+
+
 
 class AccountInvoiceLine(models.Model):
     _inherit = ["account.invoice.line"]
@@ -41,23 +66,5 @@ class AccountInvoiceLine(models.Model):
                         line_obj.write({'name':description})
         return line_obj
 
-    @api.multi
-    def _get_advertising_details_for_credit(self):
-        self.ensure_one()
-        res = []
-        if self.invoice_id.refund_invoice_id and self.product_id:
-            line_obj = self.search([('invoice_id','=',self.invoice_id.refund_invoice_id.id),('product_id','=',self.product_id.id)])
-            if line_obj.sale_line_ids:
-                #If issue_date else original invoice date
-                date = line_obj.sale_line_ids.issue_date or self.invoice_id.refund_invoice_id.date_invoice
-                date =  datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%m/%d/%Y')
-                note = line_obj.sale_line_ids.adv_issue.default_note if line_obj.sale_line_ids.adv_issue else ''
-                res.append({'issue_date':date,'issue_note':note})
-        return res
 
 
-#    so_number = fields.Char(string='Order Number')
-#    sol_issuedt = fields.Date(string='Issue Date')
-#    sol_title = fields.Char(string='Title')
-#    sol_discount = fields.Float(string='Discount', digits=dp.get_precision('Account'))
-#    sol_computed_discount = fields.Float(string='Computed Discount', digits=dp.get_precision('Account'))
