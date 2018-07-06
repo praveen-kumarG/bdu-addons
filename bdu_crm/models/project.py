47f9e1bee8541ceeb4496374bae7879b2c5b8c80
kumarinie <kumarisushmanie@gmail.com>
2018-07-06 15:24:24 +0530

    Re-Revert "PRO-1 Remove restriction of maximum choices from issue_type field in project.issue"

--- bdu_crm/models/project.py
+++ bdu_crm/models/project.py
@@ -14,3 +14,10 @@ class Task(models.Model):
 
     task_type = fields.Many2one('task.type', 'Task Type')
     date_deadline = fields.Datetime(string='Deadline', index=True, copy=False)
+
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    solution_ids = fields.Many2many('project.source', 'project_solution_rel', 'project_id', 'solution_id', string='Project Solutions')
+    source_ids = fields.Many2many('project.source', 'project_source_rel', 'project_id', 'source_id', string='Project Sources')
+    issue_type_ids = fields.Many2many('project.issue.type', 'project_issue_type_rel', 'project_id', 'issue_id', string='Project Issue Types')
