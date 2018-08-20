6b38bb59188d97a71af6d1ae601c3604a5874fe7
Dave Prosee <dave_prosee@hotmail.com>
2018-08-20 23:49:47 +0200

    adapted to WH comments

--- bdu_sale/models/bdu_sale.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class SaleAdvertisingIssue(models.Model):
-    _inherit = "sale.advertising.issue"
-    group_id = fields.Many2one('crm.team', 'Filter group')
-
