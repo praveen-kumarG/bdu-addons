198ccb90803c55bd89710e09237899dd7cd0c054
Dave Prosee <dave_prosee@hotmail.com>
2018-09-02 20:55:40 +0200

    Added security model: Pubble connector and Pubble kpi data under Technical Settings Hence providing access to the connector config and data view respectively.

--- bdu_mis_builder_pubble_kpis/__manifest__.py
+++ bdu_mis_builder_pubble_kpis/__manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/pubble_config.xml',
         'views/pubble_kpis.xml',
     ],
