5162058b8900e4d40ec73df2510a85de22f15609
Dave Prosee <dave_prosee@hotmail.com>
2018-09-13 23:24:56 +0200

    filter for analytic account and operating unit on query

--- bdu_mis_builder_pubble_kpis/models/mis_builder.py
+++ bdu_mis_builder_pubble_kpis/models/mis_builder.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
+import pdb
 from odoo import api, fields, models
 
 class MisReportInstancePeriod(models.Model):
 
     _inherit = 'mis.report.instance.period'
 
-    operating_unit_ids = fields.Many2many('operating.unit',
-                                          string='Operating Unit',
-                                          required=False)
-
     @api.multi
     def _get_additional_query_filter(self, query):
-        #let super do its job first
+        
+        #let super do its job first (although current implementation does not add anything)
         aml_domain = super(MisReportInstancePeriod, self).\
             _get_additional_query_filter(query)
-        #now additional filter under su account
+
+        #add own analytic account filter
+        analytic_account_id = self.env.context.get('analytic_account_id')
+        if analytic_account_id:
+            aml_domain.append(('analytic_account_id', '=', analytic_account_id))
+        
+        #additional filter made under su account for operating units
         sudoself = self.sudo()
         if sudoself.report_instance_id.operating_unit_ids:
             aml_domain.append(
@@ -24,4 +28,5 @@ class MisReportInstancePeriod(models.Model):
             aml_domain.append(
                 ('operating_unit_id', 'in',
                  sudoself.operating_unit_ids.ids))
+        
         return aml_domain
