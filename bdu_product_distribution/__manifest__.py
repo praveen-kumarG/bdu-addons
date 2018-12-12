a73df0d146eda22b866ed533ef59cf509b70ac78
Dave Prosee <daveprosee@Daves-MacBook-Pro.local>
2018-12-12 13:19:47 +0100

    BDU product distribution : appendix restyle, zip6 added to logistic table views

--- bdu_product_distribution/__manifest__.py
+++ bdu_product_distribution/__manifest__.py
@@ -8,10 +8,11 @@
 
     'description': """
         Product distribution enhancements include:\n
-        - no product variant selection enhancement (not needed)\n
         - custom handling on orderline form\n
         - selected area(s) and distributor added to sale orderline\n
-        - quantity ordered calculated on logistics table, divided by 1000\n\n
+        - quantity ordered calculated on logistics table, divided by 1000\n
+        - appendices for customer with distribution details\n
+        - order details per distributor (not administered)\n\n
         Usage:\n
         - define products within custom handling \'Distribution\' , found on product tab \'Sale\'\n
         - or organize products under a product category with mentioned custom handling \n
